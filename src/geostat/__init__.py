9334eb7e0be47455ed6eb5f8af050e112c36bb54
Will Chang <will@hypergradient.ai>
2022-08-11 10:07:59 -0700

    Mesh class

--- src/geostat/__init__.py
+++ src/geostat/__init__.py
@@ -1,8 +1,10 @@
 from .gp import *
 from .krige import *
 from .misc import *
+from .mesh import *
 
 __all__ = []
 __all__.extend(gp.__all__)
 __all__.extend(krige.__all__)
 __all__.extend(misc.__all__)
+__all__.extend(mesh.__all__)
