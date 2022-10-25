a3283889a8c107be3e8e998abe7d1e900d96df5e
Will Chang <will@hypergradient.ai>
2022-10-25 15:23:42 -0700

    linop.LOLowRank

--- src/geostat/linop.py
+++ src/geostat/linop.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
+from dataclasses import dataclass
 from tensorflow.linalg import LinearOperator as LO
 from tensorflow.linalg import LinearOperatorBlockDiag as LOBlockDiag
 from tensorflow.linalg import LinearOperatorComposition as LOComposition
@@ -8,8 +9,31 @@ from tensorflow.linalg import LinearOperatorDiag as LODiag
 from tensorflow.linalg import LinearOperatorFullMatrix as LOFullMatrix
 from tensorflow.linalg import LinearOperatorPermutation as LOPermutation
 
+logdet = tf.linalg.logdet
+
 def e(x, axis=-1):
     return tf.expand_dims(x, axis)
+
+def t(x, perm=None, conjugate=False):
+    return tf.transpose(x, perm, conjugate)
+
+def reshape(x, adjoint_arg):
+    s = tf.shape(x)
+    x = tf.reshape(x, [-1, s[-2], s[-1]])
+    batch_dims = s[:-2]
+    if adjoint_arg:
+        x = tf.transpose(x, [2, 0, 1], conjugate=True)
+    else:
+        x = tf.transpose(x, [1, 0, 2])
+    inner_dim = tf.shape(x)[-1]
+    x = tf.reshape(x, [tf.shape(x)[0], -1])
+    return x, batch_dims, inner_dim
+
+def revert(x, batch_dims, inner_dim):
+    x = tf.reshape(x, [tf.shape(x)[0], -1, inner_dim])
+    x = tf.transpose(x, [1, 0, 2])
+    x = tf.reshape(x, tf.concat([batch_dims, [-1, inner_dim]], axis=0))
+    return x
 
 class LODiagBlockDiag(LO):
     def __init__(self, blocksizes: np.ndarray, diag, const, dtype=tf.float32):
@@ -19,54 +43,75 @@ class LODiagBlockDiag(LO):
         self.diag = tf.cast(diag, dtype)
         self.const = tf.cast(const, dtype)
         super().__init__(dtype=dtype, is_square=True, is_self_adjoint=True)
+
     def to_block_diag(self):
         blocks = []
         for s, d, c in zip(self.blocksizes, self.diag, self.const):
             blocks.append(LOFullMatrix(tf.eye(s) * d + c))
         return LOBlockDiag(blocks)
+
     def _to_dense(self):
         return self.to_block_diag().to_dense()
+
     def _shape(self):
         return tf.TensorShape([self.size, self.size])
+
     def _matmul(self, x, adjoint=False, adjoint_arg=False):
-        s = tf.shape(x)
-        x = tf.reshape(x, [-1, s[-2], s[-1]])
-        if adjoint_arg:
-            x = tf.transpose(x, [2, 0, 1], conjugate=True)
-        else:
-            x = tf.transpose(x, [1, 0, 2])
-        a = e(e(self.const)) * tf.math.segment_sum(x, self.segment_ids)
-        b = e(e(self.diag))
+        x, bdims, idim = reshape(x, adjoint_arg)
+        a = e(self.const) * tf.math.segment_sum(x, self.segment_ids)
+        b = e(self.diag)
         x = tf.gather(a, self.segment_ids) + x * tf.gather(b, self.segment_ids)
-        x = tf.transpose(x, [1, 0, 2])
-        x = tf.reshape(x, s)
-        return x
+        return revert(x, bdims, idim)
+
     def _inverse(self, adjoint=False):
         n = tf.constant(self.blocksizes, dtype=self._dtype)
         d, c = self.diag, self.const
         ci = -c / (d * (d + n * c))
         di = 1 / d
         return LODiagBlockDiag(self.blocksizes, di, ci)
+
     def _solve(self, x, adjoint=False, adjoint_arg=False):
         return self._inverse().matmul(x, adjoint_arg=adjoint_arg)
+
     def _log_abs_determinant(self):
         n = tf.constant(self.blocksizes, dtype=self._dtype)
         d, c = self.diag, self.const
         return tf.reduce_sum(n * tf.math.log(d) + tf.math.log(1. + c * n / d), axis=0)
 
+@dataclass
 class LOLowRank(LO):
-    def __init__(self, Q: tf.Tensor, R: LO, N: LO, dtype=tf.float32):
-        """
-        Implements Q^T R^-1 Q + N.
-        R and N should be symmetric.
-        """
+    """
+    Implements Q^T R^-1 Q + N.
+    R and N should be symmetric.
+    """
+    Q: tf.Tensor
+    Ri: LO
+    N: LO
+    dtype = tf.float32
+
+    def __post_init__(self):
+        super().__init__(dtype=self.dtype, is_square=True, is_self_adjoint=True)
+
     def _to_dense(self):
-        return tf.einsum('ab,ac->bc', Q, R.solve(Q)) + N.to_dense()
+        Q, Ri, N = self.Q, self.Ri, self.N
+        return t(Q) @ Ri.solve(Q) + N.to_dense()
+
     def _shape(self):
-        return N.shape()
+        return self.N.shape
+
     def _matmul(self, x, adjoint=False, adjoint_arg=False):
-        return tf.einsum('ab,ac->bc', Q, R.solve(Q @ x)) + N @ x
+        Q, Ri, N = self.Q, self.Ri, self.N
+        return t(Q) @ Ri.solve(Q @ x) + N @ x
+
     def _solve(self, x, adjoint=False, adjoint_arg=False):
-        A = R.to_dense() + Q @ N.solve(Q, adjoint_arg=True)
+        x, bdims, idim = reshape(x, adjoint_arg)
+        Q, Ri, N = self.Q, self.Ri, self.N
+        A = Ri.to_dense() + Q @ N.solve(Q, adjoint_arg=True)
         B = tf.linalg.solve(A, Q @ N.solve(x, adjoint_arg=adjoint_arg))
-        B
+        x = N.solve(x - t(Q) @ B)
+        return revert(x, bdims, idim)
+
+    def _log_abs_determinant(self):
+        Q, Ri, N = self.Q, self.Ri, self.N
+        A = Ri.to_dense() + Q @ N.solve(Q, adjoint_arg=True)
+        return -Ri.log_abs_determinant() + N.log_abs_determinant() + tf.linalg.logdet(A)
--- tests/test_linop.py
+++ tests/test_linop.py
@@ -3,51 +3,63 @@ import numpy as np
 import tensorflow as tf
 
 def test_diag_block_diag():
+    A = lo.LODiagBlockDiag([2, 3, 4], [1., 1., 1.], [0.2, 0.3, 0.4])
+    check(A)
 
-    a = lo.LODiagBlockDiag([2, 3, 4], [1., 1., 1.], [0.2, 0.3, 0.4])
-    check(a)
+def test_low_rank():
+    num = 4
+    x = np.arange(num).astype(np.float32)
+    d2 = np.square(x[:, np.newaxis] - x[np.newaxis, :]) / 5.
+    C = np.exp(-d2)
+    R = lo.LOFullMatrix(C[:3, :3])
+    Q = C[:3, :]
+    N = lo.LODiag(0.1 * np.ones([num], dtype=np.float32))
+    A = lo.LOLowRank(Q, R, N)
+    check(A)
 
-def dense(a):
-    return lo.LOFullMatrix(a.to_dense())
+def allclose(a, b):
+    return np.allclose(a, b, atol=1e-4)
 
 def check(a):
+    a2 = lo.LOFullMatrix(a.to_dense())
+
+    # Shape
+    assert(allclose(a.shape.as_list(), a2.shape.as_list()))
+
     # Mat-vec multiply.
     n = a.domain_dimension
     b = np.random.uniform(-1., 1., [n, 1]).astype(np.float32)
-    assert(np.allclose(a @ b, dense(a) @ b))
+    assert(allclose(a @ b, a2 @ b))
 
     # Mat-mat multiply.
     n = a.domain_dimension
     b = np.random.uniform(-1., 1., [n, 3]).astype(np.float32)
-    assert(np.allclose(a @ b, dense(a) @ b))
+    assert(allclose(a @ b, a2 @ b))
 
     # Batch mat-mat multiply.
     n = a.domain_dimension
     b = np.random.uniform(-1., 1., [3, n, 3]).astype(np.float32)
     b[0, :, :] = 0.
-    assert(np.allclose(a @ b, dense(a) @ b))
+    assert(allclose(a @ b, a2 @ b))
 
     # Mat-vec solve.
     n = a.domain_dimension
     b = np.random.uniform(-1., 1., [n, 1]).astype(np.float32)
-    assert(np.allclose(a.solve(b), dense(a).solve(b)))
+    assert(allclose(a.solve(b), a2.solve(b)))
 
     # Mat-mat solve.
     n = a.domain_dimension
     b = np.random.uniform(-1., 1., [n, 3]).astype(np.float32)
-    assert(np.allclose(a.solve(b), dense(a).solve(b)))
+    assert(allclose(a.solve(b), a2.solve(b)))
 
     # Batch mat-mat solve.
     n = a.domain_dimension
     b = np.random.uniform(-1., 1., [3, n, 3]).astype(np.float32)
     b[0, :, :] = 0.
-    assert(np.allclose(a.solve(b), dense(a).solve(b)))
+    assert(allclose(a.solve(b), a2.solve(b)))
 
     # Inverse
-    assert(np.allclose(a.inverse().to_dense(), dense(a).inverse().to_dense()))
+    assert(allclose(a.inverse().to_dense(), a2.inverse().to_dense()))
 
     # Log abs determinant
-    assert(np.allclose(a.log_abs_determinant(), dense(a).log_abs_determinant()))
-
-    # Shape abs determinant
-    assert(np.allclose(a.shape.as_list(), dense(a).shape.as_list()))
+    assert(allclose(a.log_abs_determinant(), a2.log_abs_determinant()))
