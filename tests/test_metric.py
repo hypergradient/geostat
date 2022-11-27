ec08d4279c732e3fe8d92a0d7627c942918e5288
Will Chang <will@hypergradient.ai>
2022-11-27 12:27:40 -0800

    tweak unit test

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -8,20 +8,16 @@ def test_scaled_euclidean():
     # Create 100 random locations in a square centered on the origin.
     locs1 = np.random.uniform(-1., 1., [1000, 3])
 
-    # Initialize featurizer of location for trends.
-    def trend_terms(x, y, z): return z, z*z
-    featurizer = NormalizingFeaturizer(trend_terms, locs1)
     metric = gm.Euclidean(scale=[1., 1., 'zscale'])
     covariance = \
-        cf.Trend(featurizer) + \
         cf.GammaExponential(metric=metric) + \
-        cf.Delta(axes=[0, 1]) + \
+        cf.SquaredExponential(range=2., sill='alpha') + \
         cf.Noise()
 
     # Generating GP.
     gp1 = GP(
         covariance = covariance,
-        parameters = dict(alpha=1., zscale=5., range=0.5, sill=1., gamma=1., dsill=1., nugget=1.),
+        parameters = dict(alpha=1., zscale=5., range=0.5, sill=1., gamma=1., nugget=1.),
         verbose=True)
 
     # Generate data.
@@ -30,7 +26,7 @@ def test_scaled_euclidean():
     # Fit GP.
     gp2 = GP(
         covariance = covariance,
-        parameters = dict(alpha=2., zscale=1., range=1.0, sill=0.5, gamma=0.5, dsill=0.5, nugget=0.5),
+        parameters = dict(alpha=2., zscale=1., range=1.0, sill=0.5, gamma=0.5, nugget=0.5),
         hyperparameters = dict(reg=0, train_iters=500),
         verbose=True).fit(locs1, vals1)
 
