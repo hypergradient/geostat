906f78b971a62e780b1edb6397b6fe117f960c37
Will Chang <will@hypergradient.ai>
2022-11-28 00:13:56 -0800

    oops

--- src/geostat/__init__.py
+++ src/geostat/__init__.py
@@ -1,8 +1,10 @@
 from .gp import *
 from .krige import *
 from .mesh import *
+from .covfunc import Observation
 
 __all__ = []
 __all__.extend(gp.__all__)
 __all__.extend(krige.__all__)
 __all__.extend(mesh.__all__)
+__all__.extend('Observation')
