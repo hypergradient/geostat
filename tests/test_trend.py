e67fe24e0fc1da5818f15abaeea7d3059db231db
Will Chang <will@hypergradient.ai>
2023-02-01 21:08:00 -0800

    test literal coefs

--- tests/test_trend.py
+++ tests/test_trend.py
@@ -2,7 +2,7 @@ import numpy as np
 from geostat import GP, NormalizingFeaturizer, Trend
 import geostat.covfunc as cf
 
-def test_explicit_trend():
+def test_explicit_coefs():
     # Create 100 random locations in a square centered on the origin.
     locs1 = np.random.uniform(-1., 1., [500, 2])
 
@@ -13,9 +13,9 @@ def test_explicit_trend():
 
     # Generating GP.
     gp1 = GP(
-        trend = Trend(featurizer, beta=['b0', 'b1', 'b2', 'b3']),
+        trend = Trend(featurizer, beta=[1., 'b1', 'b2', 'b3']),
         covariance = covariance,
-        parameters = dict(b0=1., b1=0.5, b2=0.5, b3=0.25, range=0.5, nugget=1.),
+        parameters = dict(b1=0.5, b2=0.5, b3=0.25, range=0.5, nugget=1.),
         verbose = True)
 
     # Generate data.
@@ -23,17 +23,17 @@ def test_explicit_trend():
 
     # Fit GP.
     gp2 = GP(
-        trend = Trend(featurizer, beta=['b0', 'b1', 'b2', 'b3']),
+        trend = Trend(featurizer, beta=[1., 'b1', 'b2', 'b3']),
         covariance = covariance,
-        parameters = dict(b0=0., b1=0., b2=0., b3=0., range=1., nugget=0.5),
+        parameters = dict(b1=0., b2=0., b3=0., range=1., nugget=0.5),
         verbose = True).fit(locs1, vals1, iters=200)
     print()
 
     # MCMC.
     gp3 = GP(
-        trend = Trend(featurizer, beta=['b0', 'b1', 'b2', 'b3']),
+        trend = Trend(featurizer, beta=[1., 'b1', 'b2', 'b3']),
         covariance = covariance,
-        parameters = dict(b0=0., b1=0., b2=0., b3=0., range=1., nugget=0.5),
+        parameters = dict(b1=0., b2=0., b3=0., range=1., nugget=0.5),
         verbose = True).mcmc(locs1, vals1, burnin=500, samples=500)
 
     # Interpolate using GP.
@@ -47,7 +47,7 @@ def test_explicit_trend():
     assert np.all(mean == mean2)
     assert np.all(var == var2)
 
-def foo_test_explicit_trend():
+def test_explicit_trend():
     # Create 100 random locations in a square centered on the origin.
     locs1 = np.random.uniform(-1., 1., [500, 2])
 
