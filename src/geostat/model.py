b83f32984f897745771f8190be9e20dd8afcf30d
Emanuel-Pytlik <pytlik.em@gmail.com>
2024-09-30 15:06:29 -0700

    Testing parameters table

--- src/geostat/model.py
+++ src/geostat/model.py
@@ -560,7 +560,8 @@ class Model():
     def __post_init__(self):
         '''
         Parameters:
-            x (test, optional: None): Pandas DataFrame with columns for locations. (None)
+            x (test, optional): Pandas DataFrame with columns for locations.
+                Default: None
 
             u : A Pandas Series containing observations. Required.
 
